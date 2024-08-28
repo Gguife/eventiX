e1dc3a5e2a8f56597d600f612d59e827dabd28ff
Gguife <gguife747@gmail.com>
2024-08-27 22:17:52 -0300

    feat: Create repositories

---
+++ api/src/main/java/com/eventiX/api/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.eventiX.api.repositories;
+
+import com.eventiX.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+}
---
+++ api/src/main/java/com/eventiX/api/repositories/CouponRepository.java
@@ -0,0 +1,12 @@
+package com.eventiX.api.repositories;
+
+
+import com.eventiX.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+
+}
