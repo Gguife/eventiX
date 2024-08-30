deedcc706aa7f83d7e5028d2a0244f86d78d04ff
Gguife <gguife747@gmail.com>
2024-08-30 20:17:47 -0300

    feat: Add coupon repository and your service file

--- api/src/main/java/com/eventiX/api/repositories/CouponRepository.java
+++ api/src/main/java/com/eventiX/api/repositories/CouponRepository.java
@@ -4,9 +4,10 @@ package com.eventiX.api.repositories;
 import com.eventiX.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
-
-
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, Date currentDate);
 }
--- api/src/main/java/com/eventiX/api/service/CouponService.java
+++ api/src/main/java/com/eventiX/api/service/CouponService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,8 +21,9 @@ public class CouponService {
     @Autowired
     private EventRepository eventRepository;
 
-    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData){
-        Event event = eventRepository.findById(eventId).orElseThrow(() -> new IllegalArgumentException("Event not found"));
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
 
         Coupon coupon = new Coupon();
         coupon.setCode(couponData.code());
@@ -32,4 +34,7 @@ public class CouponService {
         return couponRepository.save(coupon);
     }
 
+    public List<Coupon> consultCoupons(UUID eventId, Date currentDate) {
+        return couponRepository.findByEventIdAndValidAfter(eventId, currentDate);
+    }
 }
