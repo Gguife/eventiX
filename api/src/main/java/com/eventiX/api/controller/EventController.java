8defda9d4bedb1d5d966816748b606605294da49
Gguife <gguife747@gmail.com>
2024-08-30 20:19:28 -0300

    feat: Add Event repository and your logic files service/controller

--- api/src/main/java/com/eventiX/api/controller/EventController.java
+++ api/src/main/java/com/eventiX/api/controller/EventController.java
@@ -2,12 +2,22 @@ package com.eventiX.api.controller;
 
 
 import com.eventiX.api.domain.event.Event;
+import com.eventiX.api.domain.event.EventDetailsDTO;
 import com.eventiX.api.domain.event.EventRequestDTO;
+import com.eventiX.api.domain.event.EventResponseDTO;
 import com.eventiX.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/event")
@@ -27,5 +37,30 @@ public class EventController {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, state, remote, eventUrl, image);
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page, size);
+        return ResponseEntity.ok(allEvents);
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDetailsDTO> getEventDetails(@PathVariable UUID eventId){
+        EventDetailsDTO eventDatails = eventService.getEventDetails(eventId);
+        return ResponseEntity.ok(eventDatails);
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<EventResponseDTO>> getFilterEvents(@RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "10") int size,
+                                                                  @RequestParam(required = false)String title,
+                                                                  @RequestParam(required = false) String city,
+                                                                  @RequestParam(required = false) String state,
+                                                                  @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+                                                                  @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate
+                                                                  ){
+        List<EventResponseDTO> events = eventService.getFilteredEvents(page, size, title, city, state, startDate, endDate);
+        return ResponseEntity.ok(events);
     }
 }
--- api/src/main/java/com/eventiX/api/service/EventService.java
+++ api/src/main/java/com/eventiX/api/service/EventService.java
@@ -1,20 +1,26 @@
 package com.eventiX.api.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.eventiX.api.domain.address.Address;
+import com.eventiX.api.domain.coupon.Coupon;
 import com.eventiX.api.domain.event.Event;
+import com.eventiX.api.domain.event.EventDetailsDTO;
 import com.eventiX.api.domain.event.EventRequestDTO;
+import com.eventiX.api.domain.event.EventResponseDTO;
 import com.eventiX.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -27,6 +33,12 @@ public class EventService {
 
     @Autowired
     private EventRepository repository;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private CouponService couponService;
 
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
@@ -45,7 +57,76 @@ public class EventService {
 
         repository.save(newEvent);
 
+        if(!data.remote()){
+            this.addressService.createAddress(data, newEvent);
+        }
+
         return newEvent;
+    }
+
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                event.getAddress() != null ? event.getAddress().getCity() : "",
+                event.getAddress() != null ? event.getAddress().getState() : "",
+                event.getRemote(),
+                event.getEventUrl(),
+                event.getImgUrl()
+        )).stream().toList();
+    }
+
+    public List<EventResponseDTO> getFilteredEvents(int page, int size, String title, String city, String state, Date startDate, Date endDate){
+        title = (title != null) ? title : "";
+        city = (city != null) ? city : "";
+        state = (state != null) ? state : "";
+        startDate = (startDate != null) ? startDate : new Date(0);
+        endDate = (endDate != null) ? endDate : new Date();
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findFilteredEvents(new Date(), title, city, state, startDate, endDate, pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                event.getAddress() != null ? event.getAddress().getCity() : "",
+                event.getAddress() != null ? event.getAddress().getState() : "",
+                event.getRemote(),
+                event.getEventUrl(),
+                event.getImgUrl()
+        )).stream().toList();
+    }
+
+    public EventDetailsDTO getEventDetails(UUID eventId) {
+        Event event = repository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Optional<Address> address = addressService.findByEventId(eventId);
+
+        List<Coupon> coupons = couponService.consultCoupons(eventId, new Date());
+
+        List<EventDetailsDTO.CouponDTO> couponDTOs = coupons.stream()
+                .map(coupon -> new EventDetailsDTO.CouponDTO(
+                        coupon.getCode(),
+                        coupon.getDiscount(),
+                        coupon.getValid()))
+                .collect(Collectors.toList());
+
+        return new EventDetailsDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getDate(),
+                event.getAddress() != null ? event.getAddress().getCity() : "",
+                event.getAddress() != null ? event.getAddress().getState() : "",
+                event.getImgUrl(),
+                event.getEventUrl(),
+                couponDTOs);
     }
 
     private String uploadImg(MultipartFile multipartFile){
